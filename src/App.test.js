e4e7c80db3df981f5310f07c2b4df44c35703942
donut1531 <love152740@gmail.com>
2021-05-11 01:28:28 +0700

    TEST_CI_V19

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-
+import { render, screen } from '@testing-library/react';
+import App from './App';
 function add (a, b) { return a * b; };
 
 var expect = require('expect.js');
 
+
+test('renders learn react link', () => {
+  render(<App />);
+});
 it('should do math', function () {
   expect(console.log(add(4, 4)))
   expect(add(4, 4)).to.equal(16);
