adf0c6522c003dccff6f0d1cc203edf03131adb2
donut1531 <love152740@gmail.com>
2021-05-11 00:43:41 +0700

    TEST_CI_V17

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
   
 import { render, screen } from '@testing-library/react';
 import App from './App';
+function add (a, b) { return a * b; };
 
-test('TEST HOME PAGE', () => {
-  render(<App />);
+var expect = require('expect.js');
+
+it('should do math', function () {
+  expect(console.log(add(4, 4)))
+  expect(add(4, 4)).to.equal(16);
 });
