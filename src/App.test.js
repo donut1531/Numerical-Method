6d3d2a027d579f341d293bde12ff98fd7b40f8f1
donut1531 <love152740@gmail.com>
2021-05-20 22:54:22 +0700

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
 import App from './App';
 var expect = require('expect.js');
 
 
-test('check "Numerical" ', () => {
+test('check render App ', () => {
   render(<App />);
-  const linkElement = screen.getByText('Numerical');
-  expect(linkElement).toBeInTheDocument();
-});
+ 
+})
