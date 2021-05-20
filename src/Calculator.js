9623dc7e548fbc2e11dc7a1af34e3698b9a16a5a
donut1531 <love152740@gmail.com>
2021-05-20 21:45:44 +0700

    uninstall_lib_regression

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,4 @@
-import { matrix } from 'mathjs';
 
-var regression = require('regression')
 var interpolationQuadratic_Poly_linear = require('interpolating-polynomial')
 const Spline = require('cubic-spline');
 const math = require('mathjs');
