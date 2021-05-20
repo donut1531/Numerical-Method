c1f2641f253b8bcaef9c94050b25ca9399d4ec6a
donut1531 <love152740@gmail.com>
2021-05-20 21:58:37 +0700

    addtest

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-function add (a, b) { return a * b; };
-
 var expect = require('expect.js');
 
 
-test('renders learn react link', () => {
+test('check "Numerical" ', () => {
   render(<App />);
-});
-it('should do math', function () {
-  expect(console.log(add(4, 4)))
-  expect(add(4, 4)).to.equal(16);
+  const linkElement = screen.getByText('Numerical');
+  expect(linkElement).toBeInTheDocument();
 });
